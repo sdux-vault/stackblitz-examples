83ac95096c305dabfa136df1726788bd2c12a9ac
Cedar Sith <sduxvault@gmail.com>
2026-04-22 08:46:19 -0500

    Added eslint

---
+++ eslint.config.cjs
@@ -0,0 +1,56 @@
+const tseslint = require('@typescript-eslint/eslint-plugin');
+const tsParser = require('@typescript-eslint/parser');
+const eslintPluginPrettier = require('eslint-plugin-prettier');
+const angular = require('@angular-eslint/eslint-plugin');
+
+module.exports = [
+  {
+    ignores: ['**/dist/**', '**/node_modules/**', '**/coverage/**']
+  },
+
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+        sourceType: 'module',
+        ecmaVersion: 'latest'
+      }
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+      '@angular-eslint': angular,
+      prettier: eslintPluginPrettier
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      'no-console': 0,
+      'prettier/prettier': 'error',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_'
+        }
+      ],
+      '@angular-eslint/component-selector': [
+        'error',
+        {
+          prefix: 'example',
+          style: 'kebab-case',
+          type: 'element'
+        }
+      ],
+      '@angular-eslint/directive-selector': [
+        'error',
+        {
+          prefix: 'example',
+          style: 'camelCase',
+          type: 'attribute'
+        }
+      ]
+    }
+  }
+];
