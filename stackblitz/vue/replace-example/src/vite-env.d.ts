b864846b8dc8c856b8ac8d5ed5580d9819792fca
SDuX Vault <sduxvault@gmail.com>
2026-06-15 08:56:17 -0500

    Added wantsCandidates to the documentation (#4)

--- stackblitz/vue/replace-example/src/vite-env.d.ts
+++ stackblitz/vue/replace-example/src/vite-env.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="vite/client" />
 
 declare module '*.vue' {
-  import type { DefineComponent } from 'vue';
-  const component: DefineComponent<object, object, unknown>;
+  import type { Component } from 'vue';
+  const component: Component;
   export default component;
 }
